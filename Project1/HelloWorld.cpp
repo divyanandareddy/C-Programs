ecf891e0b88a6d356135e21ecfa9aaf655965a11
divyanandareddy <divyananda1997@gmail.com>
2021-08-11 13:38:58 +0530

    Update HelloWorld.cpp

--- Project1/HelloWorld.cpp
+++ Project1/HelloWorld.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-int main()
+int main() // here the int is return type,means the main() returns int type
 {
-  std::cout << "Hello World\n";
-	return 0;
+  std::cout << "Hello World\n"; // Here cout is an object and the
+	return 0; // here we are returning 0, means int
 }
