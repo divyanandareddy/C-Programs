93494c998c0b462ce647439bbc2bbe0a610bb5ea
divyanandareddy <divyananda1997@gmail.com>
2021-08-11 13:47:41 +0530

    Update variables.cpp

--- Project1/variables.cpp
+++ Project1/variables.cpp
@@ -5,6 +5,9 @@ int main()
 	int a; //variable declaration
 	a = 10; //variable initialization 
 	int b = 20; // variabel declaration and initialization
+	int c = a; // assign
 	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << c << std::endl;
 	return 0;
 }
