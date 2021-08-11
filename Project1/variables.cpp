53146c02f75ffc96a72633c2f14a17d97706f045
divyanandareddy <divyananda1997@gmail.com>
2021-08-11 13:45:49 +0530

    Create variables.cpp

---
+++ Project1/variables.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	int a; //variable declaration
+	a = 10; //variable initialization 
+	int b = 20; // variabel declaration and initialization
+	std::cout << a << std::endl;
+	return 0;
+}
