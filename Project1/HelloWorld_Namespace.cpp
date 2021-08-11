ba0abc0f53f37df8ad1f744d5b9e833a7ba36a40
divyanandareddy <divyananda1997@gmail.com>
2021-08-11 13:25:27 +0530

    Update HelloWorld_Namespace.cpp

--- Project1/HelloWorld_Namespace.cpp
+++ Project1/HelloWorld_Namespace.cpp
@@ -3,10 +3,12 @@
 //using namespace std; 
 
 // Using Declaration
-using std::cout;
+using std::cout; // this we are using only for cout object.
 
 int main()
 {
+	// Using Declaration inside a function
+	//using std::cout; --> this we can use with in the function, outside of this function if we try to use this we will get compilation error.
 	cout << "Hello World\n";
 	return 0;
 }
