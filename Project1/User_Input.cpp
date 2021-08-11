370308433a0e437100b907fbd38f53ac2275cf63
divyanandareddy <divyananda1997@gmail.com>
2021-08-11 14:44:11 +0530

    Create User_Input.cpp

---
+++ Project1/User_Input.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	int a; 
+	std::cout << "Enter a value:";
+	std::cin >> a; // here cin is an object and the class for cin is "istream"
+	std::cout << "a value:" << a << std::endl;
+}
