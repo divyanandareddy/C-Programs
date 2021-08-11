99f0cc19fc0595216e6b7d7435109a000a64c519
divyanandareddy <divyananda1997@gmail.com>
2021-08-11 19:12:14 +0530

    Update char_datatype.cpp

--- Project1/char_datatype.cpp
+++ Project1/char_datatype.cpp
@@ -1,15 +1,17 @@
+#include <iostream>
+#include <climits> 
 
 int main()
 {
 	char x='a';
 
 	std::cout << "Value of x:" << x << std::endl;
-	std::cout << "Value of x:" << int(x) << std::endl; // ascii value (a-->97)
+	std::cout << "Value of x:" << (int)x << std::endl; // ascii value (a-->97)
 
 	char y = 97; // here we are giving ascii value but it wii convert it into char (97-->a)
 
 	std::cout << "Value of x:" << x << std::endl;
-	std::cout << "Value of x:" << int(x) << std::endl;
+	std::cout << "Value of x:" << (int)x << std::endl;
 		
 	std::cout << "Size of char:" << sizeof(char) << std::endl;
 
@@ -19,7 +21,7 @@ int main()
 	unsigned char z = 129;
 
 	std::cout << "Value of x:" << z << std::endl;
-	std::cout << "Value of x:" << int(z) << std::endl;
+	std::cout << "Value of x:" << (int)z << std::endl;
 
 	std::cout << "MAX Size of unsigned char:" << UCHAR_MAX << std::endl;
 
