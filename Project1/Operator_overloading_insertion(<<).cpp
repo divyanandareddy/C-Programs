437f6ff5158eb66ca39c46fd1f460b801def1c24
divyanandareddy <divyananda1997@gmail.com>
2021-08-17 12:33:09 +0530

    Create Operator_overloading_insertion(<<).cpp

---
+++ Project1/Operator_overloading_insertion(<<).cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+
+class User
+{
+	static int user_count; 
+	std::string status = "silver";
+	public:
+		static int get_user_count()
+		{
+			return user_count;
+		}
+		std::string first_name;
+		std::string last_name;
+		std::string getstatus()
+		{
+			return status;
+		}
+
+		User()
+		{
+			std::cout << "Constructor\n";
+			user_count++;
+		}
+
+		User(std::string first_name, std::string last_name, std::string status)
+		{
+			this->first_name = first_name;
+			this->last_name = last_name;
+			this->status = status;
+			user_count++;
+		}
+		~User()
+		{
+			std::cout << "Destructor\n";
+			user_count--;
+		}
+
+
+		void set_status(std::string status)
+		{
+			if (status == "Gold" || status == "Silver" || status == "Bronze")
+			{
+				this->status = status;
+			}
+			else
+			{
+				this->status = "No-status";
+			}
+		}
+		
+};
+
+int User::user_count = 0;
+
+int add_user_if_not_exists(std::vector<User> &users,User user)
+{
+	for (int i = 0; i < users.size(); i++)
+	{
+		if (users[i].first_name == user.first_name && users[i].last_name == user.last_name)
+		{
+			return i;
+		}
+		users.push_back(user);
+		return users.size() - 1;
+	}
+}
+
+std::ostream& operator << (std::ostream& output,const User user)
+{
+	output << "first name: " << user.first_name << "\nLast name: " << user.last_name<<"\n";
+	return output;
+}
+
+int main()
+{
+	User user; 
+	user.first_name = "CSCC";
+	user.last_name = "Labs";
+	user.set_status("Gold") ;
+	std::cout << user << std::endl;	
+}
