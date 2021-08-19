12ab8da95b8736fe7a88c2e417dfd19f4b555a6e
divyanandareddy <divyananda1997@gmail.com>
2021-08-19 15:43:24 +0530

    Update Pointers-and-multidimensional-arrays(2d).cpp

--- project2/Pointers-and-multidimensional-arrays(2d).cpp
+++ project2/Pointers-and-multidimensional-arrays(2d).cpp
@@ -9,8 +9,8 @@ int main()
 {
 
 	int c[3][2] = { {1,2},
-			        {3,4},
-					{5,6} };
+			{3,4},
+			{5,6} };
 	// %p means pointer value
 	printf("%p %p %p %p %p\n", c, *c, c[0], &c[0], &c[0][0]); // i got  00EFFDBC 00EFFDBC 00EFFDBC 00EFFDBC 00EFFDBC all are same
 	printf("%d\n", *(c[0] + 1)); //2
