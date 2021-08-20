f31762bdfdf475ce6119356d8b71850be60e167d
divyanandareddy <divyananda1997@gmail.com>
2021-08-20 16:47:57 +0530

    Create datatypes.cpp

---
+++ Project3/datatypes.cpp
@@ -0,0 +1,20 @@
+int main()
+{
+	int Y_O_B = 1997;
+	char gender = 'm';
+	bool isolderthan18 = true;
+	float averagegrade = 5.8;
+	double balance = 3737742832887;
+
+	std::cout << "Size of short:" << sizeof(int)<< std::endl;
+
+	std::cout << "MIN Size of int:" << INT_MIN << std::endl;
+	std::cout << "MAX Size of int:" << INT_MAX << std::endl;
+
+
+	std::cout << "MAX Size of Unsigned int:" << UINT_MAX << std::endl;
+
+	std::cout << "Size of int:" << sizeof(char) << std::endl;
+	std::cout << "Size of long:" << sizeof(long) << std::endl;
+	std::cout << "Size of long long:" << sizeof(long long) << std::endl;
+}
