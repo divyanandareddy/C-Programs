4c80773fee61703d5924e7cc71fbba2b155e0001
divyanandareddy <divyananda1997@gmail.com>
2021-08-20 16:53:00 +0530

    Create ascii.cpp

---
+++ Project3/ascii.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using std::cout; 
+
+int main()
+{
+	cout << (int)'a' << std::endl; //97
+	cout << (int)'b' << std::endl; //98
+	cout << (int)'A' << std::endl; //65
+	cout << (char)79 << std::endl; //o
+	cout << (char)67 << std::endl; //c
+
+}
