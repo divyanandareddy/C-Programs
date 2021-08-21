4cb86a69c1c71838a093ccafd8fdd16b7e24b2ef
divyanandareddy <divyananda1997@gmail.com>
2021-08-21 20:01:00 +0530

    Create Calculator-using-switch.cpp

---
+++ Project3/Calculator-using-switch.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float num1, num2;
+	char operation;
+	cout << "** Calculator **"<<endl;
+	cin >> num1 >> operation >> num2;
+	switch (operation)
+	{
+		case '+':
+			cout << num1 << operation << num2 <<" = " << num1 + num2 << endl;
+			break;
+		case '-':
+			cout << num1 << operation << num2 << " = " << num1 - num2 << endl;
+			break;
+		case '*':
+			cout << num1 << operation << num2 << " = " << num1 * num2 << endl;
+			break;
+		case '%':
+			bool isNum1Int, isNum2Int;
+			isNum1Int = ((int)num1 == num1);
+			isNum2Int =((int)num1 == num2);
+			if (isNum1Int && isNum2Int)
+			{
+				cout << num1 << operation << num2 << " = " << (int)num1 % (int)num2 << endl;
+			}
+			else
+			{
+				cout << "Not valid!";
+			}
+			break;
+		case '/':
+			cout << num1 << operation << num2 << " = " << num1 / num2 << endl;
+			break;
+		default:
+			cout << "Invalid Operation" << endl;
+
+			break;
+	}
+}
+
+
