c2ea379ef78e23e9ca786890963012d21da0e82e
divyanandareddy <divyananda1997@gmail.com>
2021-08-26 09:21:36 +0530

    Create user-input1.cpp

---
+++ Project4/user-input1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	double age;
+	std::cout << "Enter Your age: ";
+	std::cin >> age;
+
+	std::cout << "Your age is " << age << " Years old" << std::endl;
+
+	return 0;
+}
