730007cbdfc924e0fd9bb3e5a9eba62aa5415eef
divyanandareddy <divyananda1997@gmail.com>
2021-08-26 09:22:52 +0530

    Create calculator.cpp

---
+++ Project4/calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	int num1, num2;
+	std::cout << "Enter first number: ";
+	std::cin >> num1;
+
+	std::cout << "Enter Second number: ";
+	std::cin >> num2;
+
+	std::cout << num1 + num2;
+	return 0;
+}
