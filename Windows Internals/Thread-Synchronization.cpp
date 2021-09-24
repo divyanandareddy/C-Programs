3365dd758d8d2f4be4b9a116c596736c0450544f
divyanandareddy <divyananda1997@gmail.com>
2021-09-24 08:38:18 +0530

    Create Thread-Synchronization.cpp

---
+++ Windows Internals/Thread-Synchronization.cpp
@@ -0,0 +1,43 @@
+
+#include <iostream>
+#include <Windows.h>
+#include <tchar.h>
+
+int main()
+{
+	char text[256];
+
+	HANDLE hMutex = CreateMutex(nullptr, FALSE, L"MyMutex");
+
+	std::cout << "Press enter to start (PID=" << GetCurrentProcessId() << ")..." << std::endl;
+	std::cin.get();
+	printf("Running...\n");
+
+	for (int i = 0; i < 10000; i++)
+	{
+		sprintf_s(text, "Text from process %d\r\n", GetCurrentProcessId());
+
+		WaitForSingleObject(hMutex, INFINITE);
+
+		HANDLE hFile = CreateFile(_T("c:\\test\\test.txt"), GENERIC_WRITE, FILE_SHARE_READ, 0, OPEN_ALWAYS, 0, 0);
+		if (hFile == INVALID_HANDLE_VALUE)
+		{
+			DWORD err = ::GetLastError();
+			std::cout << "Error Opening File " << GetLastError() << " at index  " << i << std::endl;
+			break;
+		}
+
+		SetFilePointer(hFile, GetFileSize(hFile, NULL), NULL, FILE_BEGIN);
+		DWORD bytes;
+		WriteFile(hFile, text, strlen(text), &bytes, NULL);
+		CloseHandle(hFile);
+
+		ReleaseMutex(hMutex);
+	}
+
+	std::cout << "Done." << std::endl;
+
+	CloseHandle(hMutex);
+	return 0;
+}
+
